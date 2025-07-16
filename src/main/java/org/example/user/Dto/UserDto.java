8d12856641eb2b9bd4f8c163b4ee46701414e9bc
Renka Aghayeva <agayevarenka@gmail.com>
2025-07-17 03:23:36 +0400

    Fix duplicate constructors in UserDto

--- src/main/java/org/example/user/Dto/UserDto.java
+++ src/main/java/org/example/user/Dto/UserDto.java
@@ -1,13 +1,8 @@
 package org.example.user.Dto;
 
-
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class UserDto {
 
     private Long id;
